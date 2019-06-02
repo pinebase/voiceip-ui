fc53a5120985a0a4a1a407c959b7fd35ff3e1373
Scott Forsberg <contact.scottforsberg@gmail.com>
2019-06-01 23:33:27 -0700

    Inital Commit
    Loaded files into version control

---
+++ modules/lib/about.php
@@ -0,0 +1,4 @@
+<p>VoiceIP Gui is a telephone software suite. It is designed to configure and monitor the Asterisk open source telephone server platform.</p>
+<p>The goal an easy to administer and efficent method of deploying and maintianing a PBX.</a>
+<h3>Author</h3>
+<h3><a href="http://scottforsberg.com/">Scott Forsberg</a></h3>
